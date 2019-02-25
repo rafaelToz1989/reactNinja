0c59723fae08f566936b11a42f48e1db9fe3a194
rafaelToz1989 <rafa.tozi@hotmail.com>
2019-02-24 22:53:07 -0300

    Modulo 1 - Aula 21

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -3,24 +3,41 @@
 import React, {Component} from 'react'
 import Title from './title'
 import Button from './button'
+import Square from './square'
 import LikeButton from './like-button'
  import SearchButton from './search-button'
 
 class App extends Component{
+
+    constructor (){
+        super()
+        this.state = {
+            color: 'green'
+        }
+    }
+    
     render (){
         return(
-            <div className='container' 
-            // onClick={(e) => {
-            //     alert('clicou!')
-            // }}
-            >
-              <LikeButton />
-              <SearchButton />
+            <div>
+             {/* onClick={() => this.setState({
+                text: 'outro texto'
+            })} */}
+           
+              {/* <LikeButton />
+              <SearchButton /> */}
+
              {/* <Title name='Rafael' lastname={{first: 'Cardoso', second: 'Tozi'}}/> */}
              {/* {['blue', 'red', 'green'].map((square, index) => (
                    <Square key={index} color={square}></Square>
              ))}            */}
-            {/* <Square/> */}
+            <Square color={this.state.color} />
+
+            {['red', 'green', 'blue'].map((color) => (
+                <Button key={color} 
+                handleClick={() => this.setState({ color })}>
+                {color}
+                </Button>
+            ))}
             </div>
         )
     }
