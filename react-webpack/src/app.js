9f6070ae95c43ed4a3c7cb6546873fdeb8d16a21
rafaelTozi1989 <rafa.tozi@hotmmail.com>
2019-03-19 19:23:36 -0300

    Modulo 1 - aula 24

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,22 +1,36 @@
 'use strict'
 
 import React, {Component} from 'react'
-import Title from './title'
-import Button from './button'
-import Square from './square'
-import LikeButton from './like-button'
- import SearchButton from './search-button'
+// import Title from './title'
+// import Button from './button'
+// import Square from './square'
+// import LikeButton from './like-button'
+// import SearchButton from './search-button'
+import Timer from './timer'
 
 class App extends Component{
 
     constructor (){
+        console.log('constructor')
+            
         super()
         this.state = {
-            color: 'green'
+            // color: 'green'
+            time: 0,
+            showTimer: true
         }
     }
+
+    componentWillMount(){
+        console.log('componentWillMount')        
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount')        
+    }   
     
     render (){
+        console.log('render')
         return(
             <div>
              {/* onClick={() => this.setState({
@@ -30,14 +44,25 @@ class App extends Component{
              {/* {['blue', 'red', 'green'].map((square, index) => (
                    <Square key={index} color={square}></Square>
              ))}            */}
-            <Square color={this.state.color} />
+            {/* <Square color={this.state.color} />
 
             {['red', 'green', 'blue'].map((color) => (
                 <Button key={color} 
                 handleClick={() => this.setState({ color })}>
                 {color}
                 </Button>
-            ))}
+            ))} */}
+            
+            {/* {this.state.showTimer && <Timer time={this.state.time} />}
+            <button onClick={() => {
+                this.setState({ showTimer: !this.state.showTimer })
+            }}>Show / Hide timer</button> */}
+
+            <Timer time={this.state.time}/>
+
+            <button onClick={() => {
+                this.setState({ time: this.state.time + 10 })
+            }}>Change props</button>
             </div>
         )
     }
