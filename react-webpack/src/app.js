1dc77edf6bb3abb02ab2523dbd3db06e7e1b3a37
rafaelTozi1989 <rafa.tozi@hotmmail.com>
2019-04-01 16:40:59 -0300

    Modulo 1 - aula 33

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -10,18 +10,19 @@ import React, { Component } from 'react'
 
 class App extends Component {
 
-    // constructor() {
-    //     console.log('constructor')
-    //     super()
-    //     this.state = {
-    //         // color: 'green'
-    //         // time: 0,
-    //         // showTimer: true
-    //         value: 'Valor Inicial',
-    //         checked: false,
-    //         selected: '2'
-    //     }
-    // }
+    constructor() {
+        console.log('constructor')
+        super()
+        this.state = {
+            // color: 'green'
+            // time: 0,
+            // showTimer: true
+            // value: 'Valor Inicial',
+            // selected: '2',
+            checked: false,
+            showContent: false
+        }
+    }
 
     // componentWillMount(){
     //     console.log('componentWillMount')        
@@ -34,7 +35,7 @@ class App extends Component {
     render() {
         console.log('render')
         return (
-            <div>
+            <div className='app'>
                 {/* onClick={() => this.setState({
                 text: 'outro texto'
             })} */}
@@ -68,7 +69,7 @@ class App extends Component {
 
                 {/* <Button handleClick={() => console.log('Clicou')}>Clique em mim</Button> */}
 
-                <form onSubmit={(e) => {
+                {/* <form onSubmit={(e) => {
                     e.preventDefault()
                     console.log('event', e)
                 }}
@@ -83,9 +84,9 @@ class App extends Component {
                     <input type='name' name='name'/>
                     <input type='email' name='email' />
 
-                    <input type='checkbox' />
+                    <input type='checkbox' /> */}
 
-                    {/* <input type="text" value={this.state.value} onChange={(e) => {
+                {/* <input type="text" value={this.state.value} onChange={(e) => {
                         console.log(e.nativeEvent)
                         this.setState({
                             value: e.target.value
@@ -103,20 +104,49 @@ class App extends Component {
 
                     <input type='radio' name='rd' value='1' defaultChecked/> Radio 1
                     <input type='radio' name='rd' value='2' /> Radio 2 */}
-                    {/* <select multiple value={['1', '2']} onChange={(e) =>{
+                {/* <select multiple value={['1', '2']} onChange={(e) =>{
                         this.setState({value: e.target.selected})
                     }}>
                         <option value="1">Opção 1</option>
                         <option value="2">Opção 2</option>
                         <option value="3">Opção 3</option>
                     </select> */}
-                    {/* <textarea name='textarea' defaultValue={'textarea \nvalue'} /> */}
-                    <button type='submit'>Enviar</button>
-                </form>
+                {/* <textarea name='textarea' defaultValue={'textarea \nvalue'} /> */}
+                {/* <button type='submit'>Enviar</button>
+                </form> */}
+                {/* <label>
+                    <input type="checkbox" checked={this.state.checked} onChange={() => {
+                        this.setState({
+                            checked: !this.state.checked
+                        }, () => {
+                            this.setState({
+                                showContent: this.state.checked
+                            })
+                        })
+                    }} /> Mostrar Conteúdo
+                </label>
+
+                {this.state.showContent && <div>Olha eu aqui!</div>} */}
+
+                <div className='user-info'>
+                    <img src='https://avatars2.githubusercontent.com/u/487669?v=4' />
+                    <h2>
+                        <a href='https://github.com/fdaciuk'>Fernando Daciuk </a>
+                    </h2>
+                    <ul className='repos-info'>
+                        <li>- Repositórios: 167</li>
+                        <li>- Seguidores: 1733</li>
+                        <li>- Seguindo: 228</li>
+                    </ul>
+                    <div className='actions'>
+                        <button>Ver repositórios</button>
+                        <button>Ver favoritos</button>
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
 
-export default App;
+export default App
