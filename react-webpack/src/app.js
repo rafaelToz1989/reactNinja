9ace518b50c4cda34254641be729a7bcf774f440
rafaelTozi1989 <rafa.tozi@hotmmail.com>
2019-05-06 07:49:01 -0300

    old files

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -31,44 +31,44 @@ class App extends Component {
         }
     }
 
-    getGitHubApiUrl (username, type) {
+    getGitHubApiUrl(username, type) {
         const internalUser = username ? `/${username}` : ''
         const internalType = type ? `/${type}` : ''
         return `https://api.github.com/users${internalUser}${internalType}`
     }
 
-    handleSearch (e) {
-            const value = e.target.value
-            const keyCode = e.which || e.keyCode
-            const ENTER = 13
-            
-            e.persist()
-            
-            if (keyCode === ENTER) {
-                e.target.disable = true
-                console.log('evento: ', e) 
-                ajax().get(this.getGitHubApiUrl(value))
-                .then((result) =>{
-                  console.log(result)
-                  this.setState({
-                      userinfo: {
-                        username: result.name,
-                        photo: result.avatar_url,
-                        login: result.login,
-                        repos: result.public_repos,
-                        followers: result.followers,
-                        following: result.following
-                      },
-                      repos: [],
-                      starred: []
-                  })
-                 })
-                 .always(() => {
-                    console.log('eventos: ', e)
-                    e.target.disable = false                    
-                 })
-                } 
-            }
+    handleSearch(e) {
+        const value = e.target.value
+        const keyCode = e.which || e.keyCode
+        const ENTER = 13
+
+        e.persist()
+
+        if (keyCode === ENTER) {
+            e.target.disable = true
+            console.log('evento: ', e)
+            ajax().get(this.getGitHubApiUrl(value))
+                .then((result) => {
+                    console.log(result)
+                    this.setState({
+                        userinfo: {
+                            username: result.name,
+                            photo: result.avatar_url,
+                            login: result.login,
+                            repos: result.public_repos,
+                            followers: result.followers,
+                            following: result.following
+                        },
+                        repos: [],
+                        starred: []
+                    })
+                })
+                .always(() => {
+                    console.log('eventos: ', e) 
+                    e.target.disable = false
+                })
+        }
+    }
 
     // componentWillMount(){
     //     console.log('componentWillMount')        
@@ -90,44 +90,44 @@ class App extends Component {
 
     // }
 
-    getRepos(type){
+    getRepos(type) {
         return (e) => {
             console.log('type: ', type)
             const login = this.state.userinfo.login
             ajax().get(this.getGitHubApiUrl(login, type))
-            .then((result) =>{  
-                this.setState({
-                    [type]: result.map((repo) => ({ 
-                        name: repo.name,
-                        link: repo.html_url                 
-                    }))
+                .then((result) => {
+                    this.setState({
+                        [type]: result.map((repo) => ({
+                            name: repo.name,
+                            link: repo.html_url
+                        }))
+                    })
                 })
-            })
         }
     }
-    
+
     render() {
         console.log('render')
         return (
-            <AppContent 
-            userinfo={this.state.userinfo} 
-            repos={this.state.repos}
-            starred={this.state.starred}
-            handleSearch={(e) => this.handleSearch(e)}
-            getRepos={this.getRepos('repos')}
-            getStarred={this.getRepos('starred')}
+            <AppContent
+                userinfo={this.state.userinfo}
+                repos={this.state.repos}
+                starred={this.state.starred}
+                handleSearch={(e) => this.handleSearch(e)}
+                getRepos={this.getRepos('repos')}
+                getStarred={this.getRepos('starred')}
             />
-           //  <div className='app'>
+            //  <div className='app'>
             //    onClick={() => this.setState({
             //    text: 'outro texto'
-           // })}
+            // })}
 
-              // <LikeButton />
+            // <LikeButton />
             //  <SearchButton /> 
 
             //<Title name='Rafael' lastname={{first: 'Cardoso', second: 'Tozi'}}/> */}
             // {['blue', 'red', 'green'].map((square, index) => (
-                //    <Square key={index} color={square}></Square>
+            //    <Square key={index} color={square}></Square>
             // ))}           
             // <Square color={this.state.color} />
 
@@ -152,63 +152,63 @@ class App extends Component {
             //  <Button handleClick={() => console.log('Clicou')}>Clique em mim</Button> 
 
             // <form onSubmit={(e) => {
-                //     e.preventDefault()
-                //     console.log('event', e)
-                // }}
+            //     e.preventDefault()
+            //     console.log('event', e)
+            // }}
 
-                //     onChange={(e) => {
-                //         console.log('name', e.target.name)
-                //         console.log('value', e.target.value)
+            //     onChange={(e) => {
+            //         console.log('name', e.target.name)
+            //         console.log('value', e.target.value)
 
-                //     }}
-                // >
+            //     }}
+            // >
 
-                //     <input type='name' name='name'/>
-                //     <input type='email' name='email' />
+            //     <input type='name' name='name'/>
+            //     <input type='email' name='email' />
 
-                //     <input type='checkbox' /> 
+            //     <input type='checkbox' /> 
 
-                //  <input type="text" value={this.state.value} onChange={(e) => {
-                //         console.log(e.nativeEvent)
-                //         this.setState({
-                //             value: e.target.value
-                //         })
-                //     }} />
-                //     <label>
-                //         <input type='checkbox'
-                //             value='my-checkbox'
-                //             checked={this.state.checked}
-                //             onChange={(e) => {
-                //                 this.setState({ checked: e.target.checked })
-                //             }} />
-                //         Checkbox
-                //     </label>
+            //  <input type="text" value={this.state.value} onChange={(e) => {
+            //         console.log(e.nativeEvent)
+            //         this.setState({
+            //             value: e.target.value
+            //         })
+            //     }} />
+            //     <label>
+            //         <input type='checkbox'
+            //             value='my-checkbox'
+            //             checked={this.state.checked}
+            //             onChange={(e) => {
+            //                 this.setState({ checked: e.target.checked })
+            //             }} />
+            //         Checkbox
+            //     </label>
 
-                //     <input type='radio' name='rd' value='1' defaultChecked/> Radio 1
-                //     <input type='radio' name='rd' value='2' /> Radio 2
-                // <select multiple value={['1', '2']} onChange={(e) =>{
-                //         this.setState({value: e.target.selected})
-                //     }}>
-                //         <option value="1">Opção 1</option>
-                //         <option value="2">Opção 2</option>
-                //         <option value="3">Opção 3</option>
-                //     </select>
-                //  <textarea name='textarea' defaultValue={'textarea \nvalue'} /> 
-                //  <button type='submit'>Enviar</button>
-                // </form> 
-                //  <label>
-                //     <input type="checkbox" checked={this.state.checked} onChange={() => {
-                //         this.setState({
-                //             checked: !this.state.checked
-                //         }, () => {
-                //             this.setState({
-                //                 showContent: this.state.checked
-                //             })
-                //         })
-                //     }} /> Mostrar Conteúdo
-                // </label>
+            //     <input type='radio' name='rd' value='1' defaultChecked/> Radio 1
+            //     <input type='radio' name='rd' value='2' /> Radio 2
+            // <select multiple value={['1', '2']} onChange={(e) =>{
+            //         this.setState({value: e.target.selected})
+            //     }}>
+            //         <option value="1">Opção 1</option>
+            //         <option value="2">Opção 2</option>
+            //         <option value="3">Opção 3</option>
+            //     </select>
+            //  <textarea name='textarea' defaultValue={'textarea \nvalue'} /> 
+            //  <button type='submit'>Enviar</button>
+            // </form> 
+            //  <label>
+            //     <input type="checkbox" checked={this.state.checked} onChange={() => {
+            //         this.setState({
+            //             checked: !this.state.checked
+            //         }, () => {
+            //             this.setState({
+            //                 showContent: this.state.checked
+            //             })
+            //         })
+            //     }} /> Mostrar Conteúdo
+            // </label>
 
-                // {this.state.showContent && <div>Olha eu aqui!</div>}
+            // {this.state.showContent && <div>Olha eu aqui!</div>}
             // </div>
         )
     }
