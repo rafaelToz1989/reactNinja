dfe5bd5ddf0915bb985ca1ec00dcd97b7bf7ce8c
rafaelTozi1989 <rafa.tozi@hotmmail.com>
2019-05-07 11:45:41 -0300

    Modulo 2 - aula 7

--- m02/jest-in-practice/sum.test.js
+++ m02/jest-in-practice/sum.test.js
@@ -1,9 +1,4 @@
 'use strict'
 
-const expect = require('chai').expect
-const sum = require('./sum')
-
-it('sum should be a function', () => {
-    expect(sum).to.be.a('function')
-})
-
+import { expect } from 'chai'
+import sum from  './sum'
